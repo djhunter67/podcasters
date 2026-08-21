209d04accc2814ffb71d3a37c062f2cdd612acd6
Christerpher Hunter <djhunter67@gmail.com>
2026-08-21 19:41:05 -0400

    wip: get the date of the last build

--- ops/podcastersctl/src/commands/version_arg.rs
+++ ops/podcastersctl/src/commands/version_arg.rs
@@ -1,4 +1,4 @@
-use std::{fmt, io::Read, path, process::Command};
+use std::{fmt, process::Command};
 
 use shared::shell;
 
@@ -43,8 +43,10 @@ pub fn execute(version: &version::VerState) {
         // The current rustup target
         VerSubcommand::Production => {
             // println!("The production version information is to be provided");
-            let b_ver = get_versioned("backend").unwrap_or_else(|err| format!("Error: {err:#?}"));
-            let f_ver = get_versioned("frontend").unwrap_or_else(|err| format!("Error: {err:#?}"));
+            let b_ver =
+                shell::get_versioned("backend").unwrap_or_else(|err| format!("Error: {err:#?}"));
+            let f_ver =
+                shell::get_versioned("frontend").unwrap_or_else(|err| format!("Error: {err:#?}"));
             let git_com = match get_commit_hash() {
                 Ok(val) => val,
                 Err(err) => {
@@ -53,14 +55,11 @@ pub fn execute(version: &version::VerState) {
                 }
             };
 
-            let result = Version::new(
-                f_ver,
-                b_ver,
-                git_com,
-                String::new(),
-                String::new(),
-                String::new(),
-            );
+            let rustc = get_rustc_version().unwrap_or_else(|err| format!("Error: {err:#?}"));
+
+            let build_time = get_build_time().unwrap_or_else(|err| format!("Error: {err:#?}"));
+
+            let result = Version::new(f_ver, b_ver, git_com, build_time, rustc, String::new());
 
             println!("{result}");
         }
@@ -68,61 +67,6 @@ pub fn execute(version: &version::VerState) {
             println!("The current version information");
         }
     }
-}
-
-fn get_versioned(app: &str) -> anyhow::Result<String> {
-    // std::env!("CARGO_PKG_VERSION").to_string()
-    let workspace_root = shell::find_worspace_root()?;
-    let mut version: String = String::new();
-
-    let path = path::Path::new(&workspace_root).read_dir()?;
-    for dir in path {
-        let dir = dir?;
-        // println!("File: {}", dir.dir_name().to_string_lossy());
-        if dir.file_name().eq(app) {
-            for file in dir.path().read_dir()? {
-                let file = file?;
-
-                if file.file_name().eq("Cargo.toml") {
-                    // println!("files: {}", file?.file_name().to_string_lossy());
-                    let mut file_buf: Vec<u8> = vec![];
-
-                    // Two directories down is the target file
-                    let mut cargo = std::fs::File::open(
-                        path::Path::new(&workspace_root)
-                            .join(dir.file_name())
-                            .join(file.file_name()),
-                    )?;
-
-                    cargo.read_to_end(&mut file_buf)?;
-
-                    for line in file_buf.split(|lines| *lines == b'\n') {
-                        // println!("{:#?}", String::from_utf8_lossy(line));
-
-                        if String::from_utf8_lossy(line).contains("version") {
-                            let versioned = line
-                                .split(|letter| letter.eq(&b'='))
-                                .next_back()
-                                .expect("failed to iter the line");
-
-                            version = String::from_utf8_lossy(versioned)
-                                .trim()
-                                .trim_matches('\"')
-                                .to_string();
-
-                            // println!(
-                            // "\nVERSION: {:#?}\n",
-                            // String::from_utf8_lossy(versioned).trim().trim_matches('\"')
-                            // );
-                            break; // The target line is at the top of the file
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    Ok(version)
 }
 
 fn get_commit_hash() -> anyhow::Result<String> {
@@ -135,8 +79,31 @@ fn get_commit_hash() -> anyhow::Result<String> {
 
 impl fmt::Display for Version {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Frontend: {}\nBackend: {}\nCommit Hash: {}\nBuild Time: {}\nRustc: {}\nRustup Target: {}",  self.frontend, self.backend, self.commit_hash, self.build_time, self.rustc, self.rustup_target)
+        write!(
+            f,
+            "Frontend: {}\nBackend: {}\nCommit Hash: {}\nBuild Time: {}\nRustc: {}\nRustup Target: {}",
+            self.frontend,
+            self.backend,
+            self.commit_hash,
+            self.build_time,
+            self.rustc,
+            self.rustup_target
+        )
     }
+}
+
+fn get_rustc_version() -> anyhow::Result<String> {
+    let command = Command::new("rustc").arg("--version").output()?;
+
+    let output = String::from_utf8_lossy(&command.stdout);
+
+    // println!("Output: {output}");
+
+    Ok(output.to_string())
+}
+
+fn get_build_time() -> anyhow::Result<String> {
+    Ok(String::from("Yesterday"))
 }
 
 #[cfg(test)]
@@ -146,18 +113,6 @@ mod tests {
     use rstest::rstest;
 
     use crate::commands::version_arg::get_commit_hash;
-
-    use super::get_versioned;
-
-    #[rstest]
-    fn test_backend_version() {
-        assert!(get_versioned("backend").unwrap().starts_with('0'));
-    }
-
-    #[rstest]
-    fn test_frontend_version() {
-        assert!((get_versioned("frontend")).unwrap().starts_with('0'));
-    }
 
     #[rstest]
     fn test_git_hash() {
--- shared/src/shell/mod.rs
+++ shared/src/shell/mod.rs
@@ -39,3 +39,76 @@ pub fn find_worspace_root() -> anyhow::Result<String> {
 
     Ok(workspace_root)
 }
+
+pub fn get_versioned(app: &str) -> anyhow::Result<String> {
+    // std::env!("CARGO_PKG_VERSION").to_string()
+    let workspace_root = find_worspace_root()?;
+    let mut version: String = String::new();
+
+    let path = std::path::Path::new(&workspace_root).read_dir()?;
+    for dir in path {
+        let dir = dir?;
+        // println!("File: {}", dir.dir_name().to_string_lossy());
+        if dir.file_name().eq(app) {
+            for file in dir.path().read_dir()? {
+                let file = file?;
+
+                if file.file_name().eq("Cargo.toml") {
+                    // println!("files: {}", file?.file_name().to_string_lossy());
+                    let mut file_buf: Vec<u8> = vec![];
+
+                    // Two directories down is the target file
+                    let mut cargo = std::fs::File::open(
+                        std::path::Path::new(&workspace_root)
+                            .join(dir.file_name())
+                            .join(file.file_name()),
+                    )?;
+
+                    cargo.read_to_end(&mut file_buf)?;
+
+                    for line in file_buf.split(|lines| *lines == b'\n') {
+                        // println!("{:#?}", String::from_utf8_lossy(line));
+
+                        if String::from_utf8_lossy(line).contains("version") {
+                            let versioned = line
+                                .split(|letter| letter.eq(&b'='))
+                                .next_back()
+                                .expect("failed to iter the line");
+
+                            version = String::from_utf8_lossy(versioned)
+                                .trim()
+                                .trim_matches('\"')
+                                .to_string();
+
+                            // println!(
+                            // "\nVERSION: {:#?}\n",
+                            // String::from_utf8_lossy(versioned).trim().trim_matches('\"')
+                            // );
+                            break; // The target line is at the top of the file
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(version)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use rstest::rstest;
+
+    use super::get_versioned;
+
+    #[rstest]
+    fn test_backend_version() {
+        assert!(get_versioned("backend").unwrap().starts_with('0'));
+    }
+
+    #[rstest]
+    fn test_frontend_version() {
+        assert!((get_versioned("frontend")).unwrap().starts_with('0'));
+    }
+}
